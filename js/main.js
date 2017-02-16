775c40f3e38df2f227029f6f462bdf81005e0f06
alex <boomscrip@gmail.com>
2017-02-16 19:53:20 +0300

    prog

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
+
+// var age = prompt('Сколько вам лет?', '');
+//
+// if (age >= 18 &  age < 100) {
+//
+// } else {
+//   location.href = "https://www.9111.ru/questions/q6918321-razreshenii-prodazha-elektronnih-sigaret-nesovershennoletnim.html";
+// }
+
+
+// $(function)(){
+// 	$('.linker').on('click', function(){
+// 			$('.icons').slideToggle();
+// 	});
+// });
+
+
 $(function(){
 	$('.globe').on('click', function(){
 			$('.icons').slideToggle();
-		});
-});
+		})
+})
